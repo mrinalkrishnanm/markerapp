f135f15531dd4279faf924d84f3feb092821470a
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2017-08-11 16:11:37 +0530

    small changes made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import firebase from 'firebase';
-import firebaseDataBase from 'firebase/database';
 import Map from './Map.js';
 
 class App extends Component {
@@ -24,12 +23,13 @@ class App extends Component {
       messagingSenderId: "734907452072"
     };
     firebase.initializeApp(config);
-
+    //retrieving data from firebase
     let MarkerRef = firebase.database().ref('markers');
     MarkerRef.on('value', snapshot => {
       var markers = []
       let initialMarkers = snapshot.val().markers;
       for (var key in initialMarkers) {
+        //marker data from firebase is converted to object
         if (initialMarkers.hasOwnProperty(key)) {
           markers.push(initialMarkers[key])
         }
@@ -40,6 +40,7 @@ class App extends Component {
   
 
   setInitialMarkers(markers){
+    //initial marker object is converted into array format which can be passed as props into Map component
     var initialMarkers = []
     for(var i=0;i<markers.length;i++){
       initialMarkers.push(markers[i])
@@ -53,26 +54,26 @@ class App extends Component {
     var lattitude = this.refs.lat.value;
     var longitude = this.refs.long.value;
     var title = this.refs.key.value;
-
+    var unique = false
     var inputNodes = document.getElementsByTagName('input');
       for(var i =0;i<inputNodes.length;i++) {
         inputNodes[i].classList.remove("error");
     }
-
+    //checking if title input box is empty
     if(!title){
       setTimeout(() => {
         document.getElementsByClassName("title")[0].classList.add("error")
 
       }, 50)
     }
-
+    //checking if title is lattitude input box is empty
     if(!lattitude){
       setTimeout(() => {
         document.getElementsByClassName("lattitude")[0].classList.add("error")
 
       }, 50)
     }
-
+    //checking if longitude input box is empty
     if(!longitude){
       setTimeout(() => {
         document.getElementsByClassName("longitude")[0].classList.add("error")
@@ -83,7 +84,10 @@ class App extends Component {
     else{
       var newlattitude = parseFloat(lattitude);
       var newlongitude = parseFloat(longitude);
-      marker.push({position:{lat: newlattitude,lng: newlongitude},key:title, defaultAnimation: 2});
+      var uniqueTitle = title+Math.floor((Math.random() * 100000) + 1);
+      console.log(uniqueTitle);
+      marker.push({position:{lat: newlattitude,lng: newlongitude},key:uniqueTitle, defaultAnimation: 2});
+      //new marked data is pushed into the firabase database
       var MarkerRef = firebase.database().ref("markers/");
       var NewMarkerRef = MarkerRef.child("markers");
       NewMarkerRef.push ({
@@ -91,10 +95,10 @@ class App extends Component {
             lat: newlattitude,
             lng: newlongitude,
           },
-          key: title,
+          key: uniqueTitle,
           defaultAnimation: 2,
       });
-
+      //To read the data after pushing, just for testing purposes
       var ref = firebase.database().ref("markers/");
       ref.on("value", function(snapshot) {
         console.log(snapshot.val());
--- src/Map.js
+++ src/Map.js
@@ -14,9 +14,15 @@ class Map extends Component{
 	  }
 	
 	displayPosition(e){
-
+		//displaying intowindow on the clicked marker
 		console.log("clicked"+e.latLng);
-		this.setState({infoWindow: e.latLng,showInfo: true})
+		this.setState({infoWindow: e.latLng})
+		if(this.state.showInfo){
+			this.setState({showInfo:false})
+		}
+		if(!this.state.showInfo){
+			this.setState({showInfo:true})
+		}
 	}
 	render(){
 		const markers = this.props.markers|| []
