87c441230ef60af7c6b728244dfb49e3569653e0
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2017-08-16 12:05:47 +0530

    simple directions feature added

---
+++ src/Directions.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { DirectionsRenderer } from "react-google-maps";
+
+
+class Directions extends Component{
+	constructor() {
+	    super();
+	    this.state = {
+	    	
+	    }
+	  }
+	render(){
+		return(
+			<div>
+				
+			</div>
+			);
+	}
+}
+
+module.exports = Directions;
+
--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,7 @@
+/*global google*/
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
+import { withGoogleMap, GoogleMap, Marker, InfoWindow, DirectionsRenderer } from "react-google-maps";
+
 
 const googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=AIzaSyBdgIVbcOGjr9mdjPZ0enDXUNqzKmur91w"
 
@@ -9,10 +11,30 @@ class Map extends Component{
 	    this.state = {
 	    	map: null,
 	    	infoWindow: null,
-	    	showInfo: false
+	    	showInfo: false,
+	    	origin: { lat: 13.0827, lng: 80.2707 },
+    		destination:{ lat: 13.0827, lng: 80.2600 },
+    		directions: null,
 	    }
 	  }
 	
+	componentDidMount(){
+		const DirectionsService = new google.maps.DirectionsService();
+
+	    DirectionsService.route({
+	      origin: this.state.origin,
+	      destination: this.state.destination,
+	      travelMode: google.maps.TravelMode.DRIVING,
+	    }, (result, status) => {
+	      if (status === google.maps.DirectionsStatus.OK) {
+	        this.setState({
+	          directions: result,
+	        });
+	      } else {
+	        console.error(`error fetching directions`);
+	      }
+	    });
+	}
 	displayPosition(e){
 		//displaying intowindow on the clicked marker
 		this.setState({infoWindow: e.latLng})
@@ -44,14 +66,14 @@ class Map extends Component{
 				        
 				      </Marker>
 				    ))}
-
+ 					{this.state.directions && <DirectionsRenderer directions={this.state.directions} />}
 				    
 				    <Marker>
 				    	<InfoWindow  position={{ lat: 13.0827, lng: 80.2707 }} >
 					  		<div>Chennai <br/>lat:13.0827 <br/>long: 80.2707</div>
 
 						</InfoWindow>
-					</Marker>				
+					</Marker>		
 					{display}	
 				</GoogleMap>
 			</div>
