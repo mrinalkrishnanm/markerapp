d49de01d2b2333d32372a4656c2c838892473f48
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2017-08-18 19:23:53 +0530

    one test performed

---
+++ __tests__/App.react-test.js
@@ -0,0 +1,13 @@
+import RouteItem from '../src/RouteList/RouteItem.js';
+import React from 'react';
+import { mount } from 'enzyme';
+
+test('RouteItem received props', () => {
+  const todo = { destination:{lat: 13.0826802, lng: 80.27071840000008},
+destination_address:"Chennai, Tamil Nadu, India",source:{lat: 28.7040592, lng: 77.10249019999992},source_address:"Delhi, India" };
+  const wrapper = mount(
+    <RouteItem route={todo} />
+  );
+  const p = wrapper.find('h2');
+  expect(p.text()).toBe("Chennai, Tamil Nadu, India to Delhi, India");
+});
